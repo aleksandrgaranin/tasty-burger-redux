ca61b5b5f72a0544694cfc1e461df5c0efcc0576
aleksandGG <aleksandrgaranin89@gmail.com>
2020-09-16 18:55:48 -0700

    Created acrion Creators

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import * as actionType from '../../store/actions';
+import * as actionType from '../../store/actions/actionTypes';
 
 
 class BurgerBuilder extends Component {
