c498bef03e521e236c2bb0d391010df0d7f50829
aleksandGG <aleksandrgaranin89@gmail.com>
2020-09-20 14:14:34 -0700

    storing Token and userId in to Redux store

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   bbr: burgerBuilderReducer,
-  ordr: orderReducer
+  ordr: orderReducer,
+  auth: authReducer  
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
