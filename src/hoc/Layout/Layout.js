41e36e1fd088caacf3d45c4bfe773995f88827fa
aleksandGG <aleksandrgaranin89@gmail.com>
2020-09-20 19:46:59 -0700

    Updated the UI depending on Auth state ( Authenticate / Logout)

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import classes from './Layout.module.css';
 import Aux from '../Aux/Aux';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
@@ -23,8 +25,11 @@ class Layout extends Component{
     render(){ 
         return(
             <Aux>                
-                <Toolbar open={this.sideDrawerToggleHandler}/>
+                <Toolbar  
+                    isAuth = {this.props.isAuthenticated}
+                    open={this.sideDrawerToggleHandler}/>
                 <SideDrawer 
+                    isAuth = {this.props.isAuthenticated}
                     open={this.state.showSideDrawer} 
                     closed={this.sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
@@ -35,4 +40,10 @@ class Layout extends Component{
     }
 } 
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
