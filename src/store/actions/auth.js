971cbedaebe970a8dc807e7ac3cdafbcc6254ae4
aleksandGG <aleksandrgaranin89@gmail.com>
2020-09-21 12:19:48 -0700

    Persistent Auth State with localStorage (kiping data when refreshin page)

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,16 +25,19 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return{
         type: actionTypes.AUTH_LOGOUT
     }
 }
 
-export const checkAuthTimeout = (expirationTime) => {
+export const checkAuthTimeout = (expirationDate) => {
     return dispatch => {
         setTimeout(() => {
             dispatch(logout());
-        }, expirationTime * 1000);
+        }, expirationDate * 1000);
     };
 };
 
@@ -51,15 +54,18 @@ export const auth = (email, passwprd, isSingup) => {
             url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyArxnu--J8yb6IimZxorZJ0rhZWTvmS0gA';
         }
         axios.post(url, authData)
-            .then(response => {
-                console.log(response)
-                dispatch(authSuccess(response.data.idToken, response.data.localId))
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch(err => {
-                console.log(err)
-                dispatch(authFail(err.response.data.error))
-            })
+        .then(response => {
+            console.log(response);
+            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+            localStorage.setItem('token', response.data.idToken);
+            localStorage.setItem('expirationDate', expirationDate);
+            localStorage.setItem('userId', response.data.localId);
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
+            dispatch(checkAuthTimeout(response.data.expiresIn));
+        })
+        .catch(err => {
+            dispatch(authFail(err.response.data.error));
+        });
        
     };
 };
@@ -68,5 +74,23 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if( expirationDate <= new Date() ) {
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+            }            
+        }
     }
 }
